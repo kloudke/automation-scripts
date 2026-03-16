ec2f354574f7b33f0143fb4b11b37a0cc3821447
kiprotichgidii <gedionkiprotich3@gmail.com>
2026-03-16 10:39:07 +0300

    feat: (main.py) Update WPClient Headers

--- wordpress/main.py
+++ wordpress/main.py
@@ -64,7 +64,14 @@ class WPClient:
         self.session.auth = self.auth
         self.session.headers.update({
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "Accept": "application/json"
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Sec-Ch-Ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\"",
+            "Sec-Ch-Ua-Mobile": "?0",
+            "Sec-Ch-Ua-Platform": "\"Windows\"",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin"
         })
 
     def _request(self, method, endpoint, **kwargs):
